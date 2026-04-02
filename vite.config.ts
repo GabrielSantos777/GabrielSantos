2ea235646a102db71e5b09345379098c0c37134d
Gabriel Santos <84045805+GabrielSantos777@users.noreply.github.com>
2026-04-01 23:45:34 -0300

    configuração do nome do portfolio

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/Portfolio_Gabriel/" : "/",
+  base: mode === "production" ? "/GabrielSantos/" : "/",
   server: {
     host: "::",
     port: 8080,
